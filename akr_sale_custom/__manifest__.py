1f80c2e7efb11c62667e2362450739e7abd94b63
abhinand <abhinand.kr@ultsglobal.com>
2024-06-01 12:20:50 +0530

    [IMP]:sale_custom:To create multiple delivery orders.

---
+++ akr_sale_custom/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Sales Custom',
+    'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Customizations for Sales module to handle multiple deliveries',
+    'depends': ['sale_management', 'stock'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
